1b5a67fbb71b803a1a4deefa3b686b6e753dffe9
16ndowney <downeyn@kenyon.edu>
2018-12-09 20:42:11 -0500

    forgot to add this file

---
+++ js/keyStrokes.js
@@ -0,0 +1,45 @@
+var port=8437;
+var socket = io.connect('http://cslab.kenyon.edu:'+port+'/test-namespace');
+
+$(window).keypress(function(e) { // On Keypress, update input and emit it to the server
+	  //console.log(e.keyCode); // used for checking keyCodes
+	  
+	  var input;
+	  switch(e.keyCode){
+		  
+		
+		   
+		case 97: // A
+		   	
+			input='A';
+			//console.log(input);
+			break;
+			
+		case 100: // D
+						
+			input='D';
+			//console.log(input);
+			break;
+			
+		case 115: // S
+			
+			input='S';
+			//console.log(input);
+			break;
+			
+		case 119: // W
+			
+			input='W';
+			//console.log(input);
+			break;
+	   }
+	   socket.emit('message', {
+    	operation: 'input',
+    	input: input
+    });
+	   
+	    
+});
+	   
+
+	
